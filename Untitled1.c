39d2896b512d964bf8a8ff22e076832098f775cb
GiaBao-bacon <nguyengiabao102006bacon@gmail.com>
2025-06-28 08:43:13 +0700

    upload bài 4.0

--- Untitled1.c
+++ Untitled1.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 
-int sum(int a, int b) {
-    return a + b;
+int findMax(int arr[], int size) {
+    int max = arr[0];
+    int i;
+    for (i = 1; i < size; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
 }
 
 int main() {
-    int a, b;
-    printf("Nhap hai so: ");
-    scanf("%d %d", &a, &b);
-    printf("Tong: %d\n", sum(a, b));
+    int n;
+    printf("Nhap so phan tu: ");
+    scanf("%d", &n);
+    int arr[n];
+    int i;
+    printf("Nhap %d phan tu:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("So lon nhat: %d\n", findMax(arr, n));
     return 0;
 }
